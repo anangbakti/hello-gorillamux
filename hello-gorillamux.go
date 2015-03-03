204621df28c27a5d75f9f9f9f59158789b5c91c0
anang.bakti <anang.bakti@ptdmc.co.id>
2015-03-03 17:33:39 +0700

    add post savelocation on /entryapi/

--- hello-gorillamux.go
+++ hello-gorillamux.go
@@ -5,11 +5,17 @@ import (
 	"github.com/gorilla/mux"
 	"html/template"
 	"net/http"
+
+	//"appengine"
 )
 
 type Book struct {
 	Title  string
 	Author string
+}
+
+type Location struct {
+	Loc_Name string
 }
 
 func init() {
@@ -18,6 +24,8 @@ func init() {
 	r.HandleFunc("/posts", PostsOnlyIndexHandler)
 	r.HandleFunc("/posts/{id}", PostsIndexHandler)
 	r.HandleFunc("/view1/", View1Handler)
+	r.HandleFunc("/entryapi/", EntryApiHandler)
+	r.HandleFunc("/saveloc", SaveLocHandler)
 	http.Handle("/", r)
 }
 
@@ -31,6 +39,25 @@ func View1Handler(rw http.ResponseWriter, r *http.Request) {
 	if err := tpl.ExecuteTemplate(rw, "indexPage", book); err != nil {
 		http.Error(rw, err.Error(), http.StatusInternalServerError)
 	}
+}
+
+func EntryApiHandler(rw http.ResponseWriter, r *http.Request) {
+	rw.Header().Set("Content-Type", "text/html; charset=utf-8")
+	if err := tpl.ExecuteTemplate(rw, "entryApiPage", nil); err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func SaveLocHandler(rw http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(rw, "POST requests only", http.StatusMethodNotAllowed)
+		return
+	}
+	//c := appengine.NewContext(r)
+	//l := &Location{
+	//	Loc_Name: r.FormValue("loc_name"),
+	//}
+	fmt.Fprintln(rw, r.FormValue("loc_name"))
 }
 
 func HomeHandler(rw http.ResponseWriter, r *http.Request) {
