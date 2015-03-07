a38e695fff1e8154f9cbe4a689cc657e343f625a
anang.bakti <anang.bakti@ptdmc.co.id>
2015-03-07 15:26:35 +0700

    -save locs -view saved locs -return json locs

--- hello-gorillamux.go
+++ hello-gorillamux.go
@@ -1,12 +1,14 @@
 package hello
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"html/template"
 	"net/http"
 
-	//"appengine"
+	"appengine"
+	"appengine/datastore"
 )
 
 type Book struct {
@@ -26,10 +28,28 @@ func init() {
 	r.HandleFunc("/view1/", View1Handler)
 	r.HandleFunc("/entryapi/", EntryApiHandler)
 	r.HandleFunc("/saveloc", SaveLocHandler)
+	r.HandleFunc("/viewentry/", ViewEntryApiHandler)
+	r.HandleFunc("/locations/", AllLocHandler)
 	http.Handle("/", r)
 }
 
 var tpl = template.Must(template.ParseGlob("tmpl/*.html"))
+
+func AllLocHandler(rw http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	q := datastore.NewQuery("Location").Ancestor(LocationKey(c)).Limit(10)
+	var locs []*Location
+	if _, err := q.GetAll(c, &locs); err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	rw.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	rw.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(rw).Encode(locs); err != nil {
+		panic(err)
+	}
+}
 
 //contoh handler call tempate html
 func View1Handler(rw http.ResponseWriter, r *http.Request) {
@@ -48,16 +68,42 @@ func EntryApiHandler(rw http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func ViewEntryApiHandler(rw http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	q := datastore.NewQuery("Location").Ancestor(LocationKey(c)).Limit(10)
+	var locs []*Location
+	if _, err := q.GetAll(c, &locs); err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	rw.Header().Set("Content-Type", "text/html; charset=utf-8")
+	if err := tpl.ExecuteTemplate(rw, "viewEntryApiPage", locs); err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+	}
+}
+
 func SaveLocHandler(rw http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		http.Error(rw, "POST requests only", http.StatusMethodNotAllowed)
 		return
 	}
-	//c := appengine.NewContext(r)
-	//l := &Location{
-	//	Loc_Name: r.FormValue("loc_name"),
-	//}
-	fmt.Fprintln(rw, r.FormValue("loc_name"))
+	c := appengine.NewContext(r)
+	l := &Location{
+		Loc_Name: r.FormValue("loc_name"),
+	}
+	//fmt.Fprintln(rw, r.FormValue("loc_name"))
+	key := datastore.NewIncompleteKey(c, "Location", LocationKey(c))
+	if _, err := datastore.Put(c, key, l); err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(rw, r, "/entryapi/", http.StatusSeeOther)
+}
+
+// guestbookKey returns the key used for all guestbook entries.
+func LocationKey(c appengine.Context) *datastore.Key {
+	// The string "default_guestbook" here could be varied to have multiple guestbooks.
+	return datastore.NewKey(c, "Location", "default_loc", 0, nil)
 }
 
 func HomeHandler(rw http.ResponseWriter, r *http.Request) {
